d0b8ff802c74e17424e67d15b15b4c9f91553f32
axel-ac <abdullahceylande@gmail.com>
2024-07-23 12:56:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import InterviewAccord from './components/InterviewAccord';
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <h2>REACT Interview Questions and Answers</h2>
+      </div>
+      <div className='app'>
+        <div className='main'>
+          <InterviewAccord />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
